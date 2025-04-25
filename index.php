bbcf31498fe90c9ee144e6de25861f11d536e4cf
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-04-25 09:34:31 +0800

    phptutorial1

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include('layout/header.php');
 ?>
 
 <div style="margin-top: 50px; padding-left: 10px;">
-    <h1>Test</h1>
-    <p>Apa2 aja.</p>
+    <h1>Note</h1>
+    <p>This serve as note for study purpose. Thanks to https://www.w3schools.com/php</p>
 </div>
 
 <?php include('layout/footer.php'); ?>
